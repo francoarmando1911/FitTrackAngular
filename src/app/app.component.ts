3f1db548753351bd40dcfdde4ca153986d5afd8d
Franco Armando <francoarmando1911@gmail.com>
2025-04-13 11:32:11 -0300

    Actuaizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component'; // ⬅️ Asegurate de que esta ruta sea correcta
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SplashScreenComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'fitTrackAngular';
+  showSplash = true;
+
+  onSplashFinish() {
+    this.showSplash = false;
+  }
 }
---
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss']
+})
+export class SplashScreenComponent {
+  imageLoaded = false;
+  hide = false;
+
+  @Output() finish = new EventEmitter<void>();
+
+  onImageLoad() {
+    console.log('[SplashScreen] Imagen cargada');
+    this.imageLoaded = true;
+
+    setTimeout(() => {
+      console.log('[SplashScreen] Ocultando splash...');
+      this.hide = true;
+
+      setTimeout(() => {
+        console.log('[SplashScreen] Emitiendo finish');
+        this.finish.emit();
+      }, 500);
+    }, 2000);
+  }  
+}
+

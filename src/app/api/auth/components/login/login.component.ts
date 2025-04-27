a0ab7279ceaa6b819c3f11f69a9df9263497f525
Franco Armando <francoarmando1911@gmail.com>
2025-04-27 09:51:09 -0300

    Construccion de servicio y logica de loguin

--- src/app/api/auth/components/login/login.component.ts
+++ src/app/api/auth/components/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { RegisterService } from '../../../../services/register.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../../../services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone:true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
+  username: string = '';
+  password: string = '';
+  mensaje: string = '';
 
+  constructor(private loginService: LoginService){}
+
+  loginUsuario(): void {
+    const enterUsuario = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.loginService.loginUsuario(enterUsuario).subscribe({
+      next: (data) => {
+        console.log('Usuario logueado correctamente', data);
+        this.mensaje = 'Logueo exitoso';
+      },
+      error: (err) => {
+        console.log('Eror al registrar el usuario:', err);
+        this.mensaje = err.error?.message || err.error || 'Hubo un error al registrar el usuario';
+      }
+    });
+  }
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private apiUrl = 'http://localhost:8080/api/auth/register';
+
+  constructor(private http: HttpClient) { }
+
+  loginUsuario(usuario: { username: string, password: string }): Observable<any>{
+    return this.http.post(this.apiUrl, usuario);
+  }
+}

b582a52fabd3bccd930289e08c644bbae0981852
Franco Armando <francoarmando1911@gmail.com>
2025-05-04 09:31:35 -0300

    update days-component class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import LoginComponent from './api/auth/components/login/login.component';
 import RegisterComponent from './api/auth/components/register/register.component';
 import { HomeFitTrackComponent } from './pages/home-fit-track/home-fit-track.component';  
 import { DailyRoutineComponent } from './pages/daily-routine/daily-routine.component';
+import { DaysComponent } from './pages/days/days.component';
 
 export const routes: Routes = [
     {
@@ -23,6 +24,10 @@ export const routes: Routes = [
         path: 'register',
         loadComponent: () =>
             import('./api/auth/components/register/register.component').then(() => RegisterComponent),
+    },
+    {
+        path: 'days-component',
+        component: DaysComponent
     },
     {
         path: 'daily-routine',
--- src/app/pages/days/days.component.ts
+++ src/app/pages/days/days.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-days',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './days.component.html',
   styleUrl: './days.component.scss'
 })
 export class DaysComponent {
+
+  days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
 
 }

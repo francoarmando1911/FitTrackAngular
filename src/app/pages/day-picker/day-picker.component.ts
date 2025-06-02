0a627ebf846bd6775729d440ec20d717472d0013
Franco Armando <francoarmando1911@gmail.com>
2025-06-02 18:57:30 -0300

    update the header component and add the new page day picker component

---
+++ src/app/pages/day-picker/day-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-picker.component.html',
+  styleUrl: './day-picker.component.scss'
+})
+export class DayPickerComponent {
+
+}

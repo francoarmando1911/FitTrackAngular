14b611501e1e45d56aff14df17970c461c6f5277
Franco Armando <francoarmando1911@gmail.com>
2025-06-09 16:12:43 -0300

    Calories component

---
+++ src/app/pages/calories/calories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calories',
+  imports: [],
+  templateUrl: './calories.component.html',
+  styleUrl: './calories.component.scss'
+})
+export class CaloriesComponent {
+
+}

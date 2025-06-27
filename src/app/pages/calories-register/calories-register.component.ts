49d55b929c1ff2d4766f9ce04585284ec6a2b914
Franco Armando <francoarmando1911@gmail.com>
2025-06-27 08:00:11 -0300

    calories-register component

---
+++ src/app/pages/calories-register/calories-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calories-register',
+  imports: [],
+  templateUrl: './calories-register.component.html',
+  styleUrl: './calories-register.component.scss'
+})
+export class CaloriesRegisterComponent {
+
+}

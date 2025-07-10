033f0eba68cd5314634ae6553aba7e591d9bcd6a
Franco Armando <francoarmando1911@gmail.com>
2025-07-10 08:56:35 -0300

    progress view

---
+++ src/app/pages/progres-view/progres-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progres-view',
+  imports: [],
+  templateUrl: './progres-view.component.html',
+  styleUrl: './progres-view.component.scss'
+})
+export class ProgresViewComponent {
+
+}
